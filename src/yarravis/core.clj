cc51ccf0db905c88c4ce81707ae3843dbb46ff85
Korny Sietsma <korny@sietsma.com>
2013-06-01 11:24:21 +1000

    initial yarra fake data

--- src/yarravis/core.clj
+++ src/yarravis/core.clj
@@ -22,49 +22,62 @@
   [
    {:id 0
     :location "Mouth"
+    :height 0
     }
    {:id 1
     :location "Docklands"
+    :height 3
     }
    {:id 3
     :location "Southgate"
+    :height 5
     }
    {:id 4
     :location "South Yarra"
+    :height 10
     }
    {:id 5
     :location "Abbotsford"
+    :height 25
     }
    {:id 6
     :location "Kew"
+    :height 30
     }
    {:id 7
     :location "Heidelberg"
+    :height 45
     }
    {:id 8
     :location "Templestowe"
+    :height 55
     }
    {:id 9
     :location "Warrandyte"
+    :height 60
     }
    {:id 10
     :location "Coldstream"
+    :height 70
     }
    {:id 11
     :location "Healesville"
+    :height 80
     }
    {:id 12
     :location "Launching Place"
+    :height 95
     }
    {:id 13
     :location "Millgrove"
+    :height 100
     }
    ])
 
 (defroutes app-routes
   (GET "/" [] (resp/file-response "index.html" {:root "static"}))
   (GET "/bubble.json" [] (bubbledata))
-  (GET "/yarra.json" [] (yarradata))
+  (GET "/yarra.json" [] (seq  (yarradata)))
   (route/files "/" {:root "static"})
   (route/not-found "<h1>Page not found</h1>"))
 
